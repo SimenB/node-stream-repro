dbe9c5e1dc424c331620f7c0573dceaa6a85421b
simbekkh <simen.bekkhus@finn.no>
2017-08-29 12:46:00 +0200

    Remove express from example

--- index.js
+++ index.js
@@ -1,57 +1,35 @@
 'use strict';
 
-const { get } = require('http');
-const express = require('express');
-const request = require('request');
+const { get, createServer } = require('http');
 const pump = require('pump');
 
-const server1 = express();
-const server2 = express();
+console.log(`node version is ${process.version}`);
 
-server1.use('/endpoint', (req, res) => {
-    pump(request.get('http://localhost:3001/endpoint'), res, err => {
-        console.log(`node version is ${process.version}`);
+let innerRequest;
 
-        if (err) {
-            console.log('There was an error, yay!', err);
-            return;
-        }
+// Http server
+createServer((req, res) => {
+    res.writeHead(200);
+    setTimeout(() => {
+        innerRequest.abort();
+        res.end('Hello World\n');
+    }, 1000);
+}).listen(3000);
 
-        console.error('There should have been an error, much sadness!');
-        process.exit(1);
+// Proxy server
+createServer((req, res) => {
+    get('http://127.0.0.1:3000', inner => {
+        pump(inner, res, err => {
+            if (err) {
+                console.log('There was an error, yay!', err);
+                process.exit(0);
+            } else {
+                console.log('There should have been an error, much sadness!');
+                process.exit(1);
+            }
+        });
     });
-});
-
-let port = 3000;
-
-Promise.all(
-    [server1, server2].map(
-        server => new Promise(resolver => server.listen(port++, resolver))
-    )
-)
-    .then(() => {
-        let clientReq;
-
-        server2.use('/endpoint', (req, res) => {
-            // write header
-            res.writeHead(200);
-            setTimeout(() => {
-                clientReq.abort();
-                res.end('write something after other side is closed');
-            }, 200);
-        });
-
-        return new Promise((resolve, reject) => {
-            clientReq = get(
-                'http://localhost:3000/endpoint',
-                reject
-            ).on('error', () => {
-                setTimeout(resolve, 150);
-            });
-        });
-    })
-    .then(() => process.exit(0))
-    .catch(e => {
-        console.error(e);
-        process.exit(1);
+})
+    .listen(3001, () => {
+        innerRequest = get('http://127.0.0.1:3001');
     });
